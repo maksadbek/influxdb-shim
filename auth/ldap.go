89409a67773ab9cf8e42864df1d9670b50b01aac
Maksadbek <a.maksadbek@gmail.com>
2016-05-06 02:55:27 +0500

    LDAP source config params

--- auth/ldap.go
+++ auth/ldap.go
@@ -2,11 +2,14 @@ package ldap
 
 import "github.com/gogits/gogs/modules/auth/ldap"
 
-func NewSource(name, host, port, email string) *ldap.Source {
+func NewSource(name, host, port, email, userBase, filter, userDN string) *ldap.Source {
 	return &ldap.Source{
 		Name:          name,
 		Host:          host,
 		Port:          port,
 		AttributeMail: email,
+		UserBase:      userBase,
+		UserFilter:    filter,
+		UserDN:        userDN,
 	}
 }
