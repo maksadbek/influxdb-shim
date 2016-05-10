974b2f4171483ff198de782bf418d46220d3b545
Maksadbek <a.maksadbek@gmail.com>
2016-05-10 17:18:06 +0500

    proxy struct for ldap.Source to wrap configurations

--- auth/ldap.go
+++ auth/ldap.go
@@ -5,14 +5,49 @@ import (
 	"github.com/spf13/viper"
 )
 
-func NewSource(c *viper.Viper) *ldap.Source {
-	return &ldap.Source{
-		Name:          c.GetString("auth.ldap.name"),
-		Host:          c.GetString("auth.ldap.host"),
-		Port:          c.GetInt("auth.ldap.port"),
-		AttributeMail: c.GetString("auth.ldap.email"),
-		UserBase:      c.GetString("auth.ldap.userBase"),
-		Filter:        c.GetString("auth.ldap.filter"),
-		UserDN:        c.GetString("auth.ldap.userDN"),
+// source is the proxy for ldap.Source
+type Source struct {
+	s         *ldap.Source
+	UseBindDN bool
+}
+
+func (source *Source) Login(uid, password string) (User, bool) {
+	name, un, sn, mail, admin, logged := source.s.SearchEntry(uid, password, source.UseBindDN)
+	if !logged {
+		return User{}, logged
+	}
+	u := User{
+		Email:    mail,
+		Name:     name,
+		Username: un,
+		Surname:  sn,
+		IsAdmin:  admin,
+	}
+	return u, logged
+}
+
+type User struct {
+	Email    string
+	Name     string
+	Username string
+	Surname  string
+	IsAdmin  bool
+}
+
+func NewSource(c *viper.Viper) *Source {
+	return &Source{
+		s: &ldap.Source{
+			Name:              c.GetString("auth.ldap.name"),
+			Host:              c.GetString("auth.ldap.host"),
+			Port:              c.GetInt("auth.ldap.port"),
+			UserBase:          c.GetString("auth.ldap.userBase"),
+			Filter:            c.GetString("auth.ldap.userFilter"),
+			UserDN:            c.GetString("auth.ldap.userDN"),
+			AttributeUsername: c.GetString("auth.ldap.attrUsername"),
+			AttributeName:     c.GetString("auth.ldap.attrName"),
+			AttributeSurname:  c.GetString("auth.ldap.attrSurname"),
+			AttributeMail:     c.GetString("auth.ldap.attrMail"),
+		},
+		UseBindDN: c.GetBool("auth.ldap.useBindDN"),
 	}
 }
